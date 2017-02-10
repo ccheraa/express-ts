dd1abf99058d1f0d10d0c1d51d89ded3571a007c
Ashraf Cheraa <ccheraa@gmail.com>
2017-02-10 12:31:35 +0100

    db router: format data before returning

--- src/express-ts/db/route.ts
+++ src/express-ts/db/route.ts
@@ -17,13 +17,20 @@ function resultMulti(data, count): any {
   return resultBase({docs: data, total: count});
 }
 
-export const ModelRoutes = function (model: Model, url: string = '/'): Route[] {
+export const ModelRoutes = function (model: Model, url: string = '/', format?: express.RequestHandler): Route[] {
   console.log(url + '/find');
   let find: Route = new Route(url + '/find')
     .on('post', (req: express.Request, res: express.Response, next: express.NextFunction) => {
       DB.canAccess(model, req, res, null, () => {
         model.list(req.body.doc, req.body.fields, req.body.config).subscribe(
-          documents => model.count(req.body.doc).subscribe(count => res.json(resultMulti(documents, count))),
+          documents => model.count(req.body.doc).subscribe(count => {
+            if (format) {
+              req.body.data = documents;
+              format(req, res, () => res.json(res.json(resultMulti(req.body.data, count))));
+            } else {
+              res.json(resultMulti(documents, count));
+            }
+          }),
           err => res.json(resultError(err))
         );
       });
@@ -48,7 +55,14 @@ export const ModelRoutes = function (model: Model, url: string = '/'): Route[] {
     .on('get', (req: express.Request, res: express.Response, next: express.NextFunction) => {
       DB.canAccess(model, req, res, null, () => {
         model.list(req.query.doc).subscribe(
-          documents => model.count(req.query.doc).subscribe(count => res.json(resultMulti(documents, count))),
+          documents => model.count(req.query.doc).subscribe(count => {
+            if (format) {
+              req.body.data = documents;
+              format(req, res, () => res.json(res.json(resultMulti(req.body.data, count))));
+            } else {
+              res.json(resultMulti(documents, count));
+            }
+          }),
           err => res.json(resultError(err))
         );
       });
@@ -69,16 +83,24 @@ export const ModelRoutes = function (model: Model, url: string = '/'): Route[] {
         );
       });
     });
-  let single: Route = new Route(url + '/:id(\\d+)')
+  let single: Route = new Route(url + '/:id([0-9a-f]+)')
     .on('get', (req: express.Request, res: express.Response, next: express.NextFunction) => {
       DB.canAccess(model, req, res, null, () => {
         model.get(req.params.id, req.query.doc).subscribe(
-          document => res.json(resultOne(document)),
+          document => {
+            if (format) {
+              req.body.data = document;
+              format(req, res, () => res.json(resultOne(req.body.data)));
+            } else {
+              res.json(resultOne(document));
+            }
+          },
           err => res.json(resultError(err))
         );
       });
     })
     .on(['put', 'post'], (req: express.Request, res: express.Response, next: express.NextFunction) => {
+      console.log(req.body.doc);
       DB.canAccess(model, req, res, null, () => {
         model.set(req.params.id, req.body.doc).subscribe(
           result => res.json(resultOne(result)),
@@ -96,6 +118,6 @@ export const ModelRoutes = function (model: Model, url: string = '/'): Route[] {
     });
   return [single, multi, find];
 };
-export const ModelController = function (model: Model, baseurl: string = '/', url: string = '/'): Controller {
-  return new Controller(baseurl, ModelRoutes(model, url));
+export const ModelController = function (model: Model, baseurl: string = '/', url: string = '/', format?: express.RequestHandler): Controller {
+  return new Controller(baseurl, ModelRoutes(model, url, format));
 };
